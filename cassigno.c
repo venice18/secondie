1cafce3214e8a6da286b6ea918f8ac791a6c9f43
venice18 <ndirituliz34@gmail.com>
2024-09-25 12:53:55 +0300

    simple interest

---
+++ cassigno.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+//program to calculate simple interest
+int main(){
+  int principal_amount;
+  int time;
+  float rate_values;
+  int Simple_interest;
+  //calculting simple interest
+  Simple_interest = (principal_amount * time * rate_values)/100;
+
+  //printing the principal amount
+  printf("Enter the principal_amount:\n ");
+  scanf("%d", &principal_amount);
+
+//printing the time
+  printf("Enter the time:\n ");
+  scanf("%d", &time);
+
+  
+  //printing the rate per annum
+  printf("Enter the rate per annum:\n ");
+  scanf("%f", &rate_values);
+
+  
+  // Output the result
+  printf("The Simple Interest is %d", Simple_interest);
+
+  
+  return 0;
+}
