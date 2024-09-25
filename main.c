aae34edd26169bcbabb5a4de4ac3ebe35fdf727e
venice18 <ndirituliz34@gmail.com>
2024-09-25 07:54:36 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+//creating variables
+int length = 5;
+int width = 10;
+int area;
+
+//calculating the area of a rectangle
+area = length * width;
+
+//print the variables
+printf("Length is %d\n", length);
+printf("Width is %d\n", width);
+printf("Area of the rectangle is %d", area);
+
+return 0;
+}
