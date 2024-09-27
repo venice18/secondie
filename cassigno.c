47961d7fd853b7c87afed08490db5344df10c5eb
venice18 <ndirituliz34@gmail.com>
2024-09-27 18:27:15 +0300

    simple interest

--- cassigno.c
+++ cassigno.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*Author- Elizabeth Muthoni
-  Registration number-CT101/G/22818/2024
-  */
+  Registration number-CT101/G/22818/24
+  Date - 2024/09/27*/
 
 
 //program to calculate simple interest
