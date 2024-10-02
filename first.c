156378a3b14a903f89564785745d499bba5f2274
venice18 <160186478+venice18@users.noreply.github.com>
2024-10-02 17:23:12 +0300

    Update first.c

--- first.c
+++ first.c
@@ -21,4 +21,5 @@ if(age >= 21, annual_income >= 21000){
   printf("Congratulations you qualify for a loan");
 }else{
   printf("Unfortunately, we are unable to offer you a loan at this time");
+  return 0;
 }
