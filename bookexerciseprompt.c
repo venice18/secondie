cfeff79d7f61d7785b2cf886a1d1c5822de9941b
venice18 <160186478+venice18@users.noreply.github.com>
2024-10-24 23:44:08 +0300

    bookexerciseprompt.c

--- bookexerciseprompt.c
+++ bookexerciseprompt.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//Defining structure book to represent information about a book
 struct book{
     char title [30];
     char author [30];
@@ -12,19 +13,24 @@ struct book{
     float price;
 } book1;
 int main(){
-    
+
+  //prompting the user to enter the title
     printf("Enter the title of the book: ");
     scanf("%s", &book1.title);
-    
+
+  //prompting the user to enter the author
     printf("Enter the author of the book: ");
     scanf("%s", &book1.author);
-    
+
+  //prompting the user to enter the ISBN
     printf("Enter the ISBN: ");
     scanf("%s", &book1.ISBN);
-    
+
+  //prompting the user to enter the publication year 
     printf("Enter the publication year of the book: ");
     scanf("%d", &book1.publication_year);
-    
+
+  //prompting the user to enter the price
     printf("Enter the price: ");
     scanf("%f", &book1.price);
     
